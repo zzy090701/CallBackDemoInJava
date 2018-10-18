4eef2472e1162d709cc647d98f0b48749bb6c2ec
zzy090701 <1799189932@qq.com>
2018-10-18 22:31:24 +0800

    step4

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        int a = 1, b = 1;
+        int a = 123, b = 789;
         Student student = new Student("小明");
         student.fillBlank(a, b);
     }
